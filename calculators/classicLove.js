a6d842e0eb99768da6e368c3aa32324ac5eab9d8
Andrew <andrewmcrobinson@gmail.com>
2021-03-30 21:52:42 +0200

    cleanup

--- calculators/classicLove.js
+++ calculators/classicLove.js
@@ -3,11 +3,6 @@ const logger = require('../logger');
 //Java code found at https://www.codebrainer.com/blog/love-calculator-android
 //manually converted to js and tested via classicLove.test.js
 
-//22 11111 22 1
-//34322
-//563
-// 86%
-
 function countChars(name1, name2) {
   const combinedString = name1.toLowerCase() + "loves" + name2.toLowerCase();
 
@@ -16,22 +11,14 @@ function countChars(name1, name2) {
 
   for(const c1 of combinedString) {
     if (strAllChars.indexOf(c1) < 0) {
-      // console.log("found at least one instance of "+c1)
       let count = 0;
       for (const c2 of combinedString) {
-        // console.log("comparing c1:"+c1+" to c2:"+c2);
         if (c1 ==c2) {
           count = count + 1;
-          // console.log("found c1==c2 = "+c1+", count incremented to:"+count);
         }
       }
-      // console.log("adding c1:"+c1+"to strAllChars:"+strAllChars);
       strAllChars = strAllChars + c1;
-      // console.log("strAllChars now:"+strAllChars);
-      // console.log("adding:"+count.toString()+" to strCount:"+strCount);
       strCount = strCount + count.toString();
-      // console.log("strCount after:"+strCount+"\n\n");
-
     }
   }
 
@@ -46,42 +33,22 @@ function shortenNumber(str) {
     const int1 = parseInt(str.charAt(0))
     const int2 = parseInt(str.charAt(str.length-1))    
     const sumOfInts = (int1 + int2).toString();
-    // console.log("adding int1:"+int1+" and int2:"+int2+", so sumOfInts is:"+sumOfInts);
     shortenString = sumOfInts+shortenNumber(
       str.substring(1, (str.length - 1))
       );
-
-    // console.log("shortenString becomes sumOfInts:"+sumOfInts+" plus recursive so:"+shortenString);
   } else {
-    // console.log("str length <2 so returning str:"+str);
     return str;
   }
-
-  // console.log("returning shortenString:"+shortenString);
   return shortenString;
 }
 
 function calculate(str1, str2) {
   let shortString = countChars(str1, str2);
-  // let output = shortString;
   do {
-    // output = output + "\n";
     shortString = shortenNumber(shortString);
-    // if (shortString.length == 2) {
-    //   output = output + "\n";
-    // }
-    // output = output + shortString;
   } while (shortString.length > 2);
-
-  // output = output + "%";
-
-  // console.log("returning output:"+output)
-  // return output;
   return shortString;
 }
-
-
-
 
 const classicLove = {
 
@@ -91,15 +58,13 @@ const classicLove = {
   //exposed only so I could easily unit test in isolation
   shortenNumber: shortenNumber,
 
+  //exposed only so I could easily unit test in isolation
   calculate: calculate,
 
   getScore: function(personOne, personTwo) {
 
     const percentage = calculate(personOne.name, personTwo.name);
-    
-    logger.info(`percentage:${percentage}`);
-
-
+  
     return {
       "calculatorName": "classicLove",
       "score": percentage,
--- calculators/classicLove.test.js
+++ calculators/classicLove.test.js
@@ -3,6 +3,34 @@ const calculator = require("./classicLove")
 
 describe("classicLove", function() {
 
+    //this is the highest level function and the only one that needs to be 'public'
+    describe("getScore", function() {
+        it("should return a score of 86 out of 100 for objects with name: Mary and name: James", async function() {
+    
+          const p1 = {
+            "name": "Mary"
+          }
+          const p2 = {
+            "name": "James"
+          }
+                   
+          const ret = calculator.getScore(p1,p2)
+    
+          assert.equal(86, ret.score);
+          assert.equal(100, ret.maximum);
+    
+        });
+      });
+
+    //this is the next highest and could be "private" except I wanted to test from one place
+    describe("calculate", function() {
+        it("should return 86 for Mary, James", async function() {
+            assert.equal("86", calculator.calculate("Mary", "James"));
+        });
+
+    });
+
+    //this is stage 1 of converting the inputs
     describe("countChars", function() {
         it("should return 2211111221 for Mary loves James", async function() {
             assert.equal("2211111221", calculator.countChars("Mary", "James"));
@@ -12,6 +40,7 @@ describe("classicLove", function() {
         });
     });
 
+    //this is stage 2 of converting the inputs. 'calculate' calls this recursively until 2 digits remain
     describe("shortenNumber", function() {
         it("should shorten 2211111221 to 34322", async function() {
             assert.equal("34322", calculator.shortenNumber("2211111221"));
@@ -24,29 +53,8 @@ describe("classicLove", function() {
         });
     });
 
-    describe("calculate", function() {
-        it("should return 86 for Mary, James", async function() {
-            assert.equal("86", calculator.calculate("Mary", "James"));
-        });
 
-    });
 
-  describe("getScore", function() {
-    it("should return a score of 86 out of 100 for objects with name: Mary and name: James", async function() {
 
-      const p1 = {
-        "name": "Mary"
-      }
-      const p2 = {
-        "name": "James"
-      }
-               
-      const ret = calculator.getScore(p1,p2)
-
-      assert.equal(86, ret.score);
-      assert.equal(100, ret.maximum);
-
-    });
-  });
 
 });
