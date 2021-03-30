c4063f6d7f73384f366970f52ef0d421841bb88e
Andrew <andrewmcrobinson@gmail.com>
2021-03-30 21:43:48 +0200

    impl java to js

--- calculators/classicLove.js
+++ calculators/classicLove.js
@@ -3,22 +3,35 @@ const logger = require('../logger');
 //Java code found at https://www.codebrainer.com/blog/love-calculator-android
 //manually converted to js and tested via classicLove.test.js
 
+//22 11111 22 1
+//34322
+//563
+// 86%
+
 function countChars(name1, name2) {
-  const combinedString = name1 + " loves " + name2;
+  const combinedString = name1.toLowerCase() + "loves" + name2.toLowerCase();
 
   let strAllChars = "";
   let strCount = "";
 
   for(const c1 of combinedString) {
     if (strAllChars.indexOf(c1) < 0) {
+      // console.log("found at least one instance of "+c1)
       let count = 0;
       for (const c2 of combinedString) {
-        if (c1 === c2) {
+        // console.log("comparing c1:"+c1+" to c2:"+c2);
+        if (c1 ==c2) {
           count = count + 1;
+          // console.log("found c1==c2 = "+c1+", count incremented to:"+count);
         }
       }
+      // console.log("adding c1:"+c1+"to strAllChars:"+strAllChars);
       strAllChars = strAllChars + c1;
+      // console.log("strAllChars now:"+strAllChars);
+      // console.log("adding:"+count.toString()+" to strCount:"+strCount);
       strCount = strCount + count.toString();
+      // console.log("strCount after:"+strCount+"\n\n");
+
     }
   }
 
@@ -26,41 +39,45 @@ function countChars(name1, name2) {
 
 }
 
-
-// String shortenNumber(String str) {
-//   String shortenString = "";
-//   if (str.length() >= 2) {
-//     int int1 = Integer.parseInt(String.valueOf(str.toCharArray()[0]));
-//     int int2 = Integer.parseInt(String.valueOf(str.toCharArray()[str.length() - 1]));
-//     shortenString = String.valueOf(int1 + int2) + shortenNumber(str.substring(1, str.length() - 1));
-//   } else {
-//     return str;
-//   }
-
-//   return shortenString;
-// }
-
-
 function shortenNumber(str) {
   console.log("shortenNumber called with:"+str);
-
-
   let shortenString = "";
-
   if (str.length >= 2) {
-
     const int1 = parseInt(str.charAt(0))
     const int2 = parseInt(str.charAt(str.length-1))    
-    const prefix = (int1 + int2).toString();
-    console.log("adding int1:"+int1+" and int2:"+int2+", so prefix is:"+prefix);
-    shortenString = prefix+shortenNumber(str.substring(1, (str.length - 1)));;
-    console.log("shortenString becomes:"+shortenString);
+    const sumOfInts = (int1 + int2).toString();
+    // console.log("adding int1:"+int1+" and int2:"+int2+", so sumOfInts is:"+sumOfInts);
+    shortenString = sumOfInts+shortenNumber(
+      str.substring(1, (str.length - 1))
+      );
+
+    // console.log("shortenString becomes sumOfInts:"+sumOfInts+" plus recursive so:"+shortenString);
   } else {
+    // console.log("str length <2 so returning str:"+str);
     return str;
   }
 
-  console.log("returning shortenString:"+shortenString);
+  // console.log("returning shortenString:"+shortenString);
   return shortenString;
+}
+
+function calculate(str1, str2) {
+  let shortString = countChars(str1, str2);
+  // let output = shortString;
+  do {
+    // output = output + "\n";
+    shortString = shortenNumber(shortString);
+    // if (shortString.length == 2) {
+    //   output = output + "\n";
+    // }
+    // output = output + shortString;
+  } while (shortString.length > 2);
+
+  // output = output + "%";
+
+  // console.log("returning output:"+output)
+  // return output;
+  return shortString;
 }
 
 
@@ -68,18 +85,24 @@ function shortenNumber(str) {
 
 const classicLove = {
 
+  //exposed only so I could easily unit test in isolation
+  countChars: countChars,
+
+  //exposed only so I could easily unit test in isolation
+  shortenNumber: shortenNumber,
+
+  calculate: calculate,
+
   getScore: function(personOne, personTwo) {
 
-    // console.log("before");
-    const calculation = countChars(personOne.name, personTwo.name);
-    // console.log(`calculation:${calculation}`);
-    logger.info(`calculation:${calculation}`);
-    const total = shortenNumber(calculation);
-    logger.info(`total from shortenNumber:${total}`);
+    const percentage = calculate(personOne.name, personTwo.name);
+    
+    logger.info(`percentage:${percentage}`);
+
 
     return {
       "calculatorName": "classicLove",
-      "score": total,
+      "score": percentage,
       "maximum": 100
     }
   },
--- calculators/classicLove.test.js
+++ calculators/classicLove.test.js
@@ -2,8 +2,37 @@ const assert = require('assert');
 const calculator = require("./classicLove")
 
 describe("classicLove", function() {
+
+    describe("countChars", function() {
+        it("should return 2211111221 for Mary loves James", async function() {
+            assert.equal("2211111221", calculator.countChars("Mary", "James"));
+        });
+        it("should return 2211111221 for mary loves james", async function() {
+            assert.equal("2211111221", calculator.countChars("mary", "james"));
+        });
+    });
+
+    describe("shortenNumber", function() {
+        it("should shorten 2211111221 to 34322", async function() {
+            assert.equal("34322", calculator.shortenNumber("2211111221"));
+        });
+        it("should shorten 34322 to 563", async function() {
+            assert.equal("563", calculator.shortenNumber("34322"));
+        });
+        it("should shorten 563 to 86", async function() {
+            assert.equal("86", calculator.shortenNumber("563"));
+        });
+    });
+
+    describe("calculate", function() {
+        it("should return 86 for Mary, James", async function() {
+            assert.equal("86", calculator.calculate("Mary", "James"));
+        });
+
+    });
+
   describe("getScore", function() {
-    it("should return 89 out of 100 for Mary loves James", async function() {
+    it("should return a score of 86 out of 100 for objects with name: Mary and name: James", async function() {
 
       const p1 = {
         "name": "Mary"
@@ -14,9 +43,10 @@ describe("classicLove", function() {
                
       const ret = calculator.getScore(p1,p2)
 
-      assert.equal(89, ret.score);
+      assert.equal(86, ret.score);
       assert.equal(100, ret.maximum);
 
     });
   });
+
 });
