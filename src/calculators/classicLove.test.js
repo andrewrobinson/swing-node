ac7205a62113a70222400745e6077e39d7dc244e
Andrew <andrewmcrobinson@gmail.com>
2021-04-08 23:58:02 +0200

    sinon working on instance now

--- src/calculators/classicLove.test.js
+++ src/calculators/classicLove.test.js
@@ -8,10 +8,10 @@ describe("classicLove", function() {
 
     describe("getScore", function() {
 
-      const calc = new ClassicLove(new ClassicLoveHelper());
-
         it("should return a score of 86 out of 100 for objects with name: Mary and name: James", async function() {
-    
+
+          const calc = new ClassicLove(new ClassicLoveHelper());
+
           const p1 = {
             "name": "Mary"
           }
@@ -32,16 +32,11 @@ describe("classicLove", function() {
 
           it("should return a score of 100 out of 100 when calculatorHelper returns > 100", async function() {
 
-            //sinon seems to pollute all instances of this, hence other tests fail
-            // const stubbedCalculatorHelper = sinon.stub(calculatorHelper, "calculate").returns("250");
+            const helperStub = new ClassicLoveHelper()
+            sinon.stub(helperStub, 'calculate').callsFake(() => "250");
+            // sinon.stub(helper, 'calculate').returns("250")
 
-            const stubbedCalculatorHelper =  {
-              calculate: function() {
-                return "120"
-              }
-            }
-
-            const calcu = new ClassicLove(stubbedCalculatorHelper);
+            const calcu = new ClassicLove(helperStub);
       
             const p1 = {
               "name": "Mary"
@@ -54,8 +49,6 @@ describe("classicLove", function() {
       
             assert.equal(100, ret.score);
             assert.equal(100, ret.maximum);
-
-            // stubbedCalculatorHelper.restore();
       
           });
         });
