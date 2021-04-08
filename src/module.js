0e0ea08ab3d571a64b28afd5aa2be32af159365f
Andrew <andrewmcrobinson@gmail.com>
2021-04-08 20:20:21 +0200

    switch to type module and start importing classes

---
+++ src/module.js
@@ -0,0 +1,18 @@
+//module.js
+export default class {
+    constructor() {
+      console.log("GOOD");
+    }
+  }
+  
+  export class Foo {
+    constructor() {
+      console.log("FOO");
+    }
+  }
+  
+  export const url = "http://www.kaplankomputing.com";
+  
+  export function bar() {
+    console.log("bar");
+  }
--- src/server.js
+++ src/server.js
@@ -1,18 +1,29 @@
 'use strict';
 
-const express = require('express');
-const handler = require('./handler');
+import Custom, {Foo, url, bar} from './module.js';
+import Rectangle from './rectangle.js';
 
-const classicLoveHelper = require('./calculators/helpers/classicLoveHelper');
-const classicLove = require('./calculators/classicLove');
-const fakeCalculator2 = require('./calculators/fake/fake_calculator2');
-const fakeCalculator3 = require('./calculators/fake/fake_calculator3');
+import express from 'express';
+// var router = express.Router();
+
+// import handler from './handler';
+// const classicLoveHelper = require('./calculators/helpers/classicLoveHelper');
+// const classicLove = require('./calculators/classicLove');
+// const fakeCalculator2 = require('./calculators/fake/fake_calculator2');
+// const fakeCalculator3 = require('./calculators/fake/fake_calculator3');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
 
+
+
+
+
+
+
+  
 /*
 
 See handler.test.js for proof of  0.89 x 0.2 + 0.5 x 0.5 + 0.0 x 0.3 = 0.428 behaviour
@@ -23,20 +34,30 @@ curl -H "Content-Type: application/json" -d '{"personOne": {"name": "Mary"},"per
 
 */
 
-const calculators = [
-	{"weight": 0.2, "fn": classicLove(classicLoveHelper)}, //weight gets bumped to 1 if only a single calculator is enabled
-	// {"weight": 0.5, "fn": fakeCalculator2}, 
-	// {"weight": 0.3, "fn": fakeCalculator3}
-];
+// const calculators = [
+// 	{"weight": 0.2, "fn": classicLove(classicLoveHelper)}, //weight gets bumped to 1 if only a single calculator is enabled
+// 	// {"weight": 0.5, "fn": fakeCalculator2}, 
+// 	// {"weight": 0.3, "fn": fakeCalculator3}
+// ];
 
-const myHandler = handler(calculators);
+// const myHandler = handler(calculators);
 
 app.use(express.json());
 
 app.post('/', (req, res) => {
 	const personOne = req.body.personOne;
 	const personTwo = req.body.personTwo;
-	res.json(myHandler.handlePost(personOne, personTwo));
+	const square = new Rectangle(10, 10);
+	console.log(`XXX square area: ${square.area}`); // 100
+
+	const custom = new Custom();
+	const foo = new Foo();
+	bar();
+	console.log(url);
+
+	res.json({a:"b"});
+
+	// res.json(myHandler.handlePost(personOne, personTwo));
 });
 
 app.listen(PORT, HOST);
